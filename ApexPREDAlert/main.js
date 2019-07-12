d21d3180058cd05905b0deef968a5ed952693184
AnArChY2122 <cmwilson919@gmail.com>
2019-07-12 16:24:14 -0400

    Initial commit

---
+++ ApexPREDAlert/main.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var client = require("mongodb").MongoClient;
+client.connect("mongodb://apex-db:xBVVJs84DgyIxctZqgSfTfHJ48B8RgsNJk7uZLkwlikRv5PSaZ6pXmKcHva7OKJ4f2lJUE65SNhdSKVDibOZCA%3D%3D@apex-db.documents.azure.com:10255/?ssl=true", function (err, client) {
+  client.close();
+});
+var a = document.getElementsByName("addressofSighting");
+var n = document.getElementsByName("NameofPerson");
+var t = document.getElementsByName("timeofday");
+document.onload = client.connect();
+document.getElementById("Submit Sighting").onclick = doverb();
+const db = client.db('Preds');
+const collection = db.collection('Predator');
+function doverb(){
+    collection.insertOne({name: n}, {address: a}, {time: t}, (err, result) => {});
+}
+
