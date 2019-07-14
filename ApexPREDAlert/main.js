b060389a9f12dc2c3e9d112b0831b47f675bf277
AnArChY2122 <cmwilson919@gmail.com>
2019-07-13 21:47:02 -0400

    updated path

--- ApexPREDAlert/main.js
+++ ApexPREDAlert/main.js
@@ -3,14 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var http= require('http');
-var server = http.createServer(function(request, response){
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("Hello Azure!");
-});
-var port = process.env.PORT || 1337;
-server.listen(port);
-console.log("Server running at http://localhost:%d", port);
 var client = require("mongodb").MongoClient;
 client.connect("mongodb://apex-db:xBVVJs84DgyIxctZqgSfTfHJ48B8RgsNJk7uZLkwlikRv5PSaZ6pXmKcHva7OKJ4f2lJUE65SNhdSKVDibOZCA%3D%3D@apex-db.documents.azure.com:10255/?ssl=true", function (err, client) {
   client.close();
